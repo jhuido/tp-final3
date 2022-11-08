fa4048cf4444d90e6d5b6da93567ed732b3c481e
jhuido <josefinahuidobro@uca.edu.ar>
2022-11-08 16:31:29 -0300

    'TDDIteration'

---
+++ src/Proceso.js
@@ -0,0 +1,5 @@
+function Proceso(){
+    this.nombre="Cola de salida";
+}
+
+module.exports=Proceso;
--- test/sistema.test.js
+++ test/sistema.test.js
@@ -1,3 +1,5 @@
+const Proceso=require("../src/Proceso");
+
 test("Crear proceso",()=>{
     var proceso=new Proceso()
     expect(proceso.nombre).toBe("Cola de salida");
