520122efdf98aba1c27c0fd268d7452e2c22302e
jhuido <josefinahuidobro@uca.edu.ar>
2022-11-08 16:48:20 -0300

    'TDDIteration'

--- test/sistema.test.js
+++ test/sistema.test.js
@@ -4,3 +4,8 @@ test("Crear proceso",()=>{
     var proceso=new Proceso(4);
     expect(proceso.cantInstancias).toBe(4);
 })
+
+test("Obtener nombre del Local",()=>{
+    var proceso=new Proceso(4);
+    expect(proceso.local.nombre).toBe("Local");
+})
