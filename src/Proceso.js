1015ba8866647d193f991f8b6fb04e1fe7306c39
jhuido <josefinahuidobro@uca.edu.ar>
2022-11-08 16:56:16 -0300

    'TDDIteration'

--- src/Proceso.js
+++ src/Proceso.js
@@ -1,8 +1,10 @@
 const Local = require("./Local");
+const Destino = require("./Destino");
 
 function Proceso(cantInstancias){
     this.cantInstancias=cantInstancias;
     this.local=new Local("Local");
+    this.destino=new Destino("Destino");
 }
 
 module.exports=Proceso;
