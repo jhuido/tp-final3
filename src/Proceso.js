544c7f34e01986683eb6d62cece30b2201c60ec3
jhuido <josefinahuidobro@uca.edu.ar>
2022-11-11 11:05:01 -0300

    'TDDIteration'

--- src/Proceso.js
+++ src/Proceso.js
@@ -24,8 +24,8 @@ function Proceso(filas,instancias){
 
     this.ubicarPaquete=function(paquete){
         var ubicacion=[]; // in(0)=fila in(1)=columna
-        for(i=0;i<this.filas;i++){
-            for(j=0;j<this.cantInstancias;j++){
+        for(var i=0;i<this.filas;i++){
+            for(var j=0;j<this.cantInstancias;j++){
                 if(this.mapa[i][j]==paquete){
                     ubicacion=[i,j];
                     return ubicacion;
