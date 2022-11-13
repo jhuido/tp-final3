1baa2faa0e878263247d1495514009141a866dc4
jhuido <josefinahuidobro@uca.edu.ar>
2022-11-13 12:57:31 -0300

    'TDDIteration'

--- test/intancia.test.js
+++ test/intancia.test.js
@@ -25,10 +25,10 @@ test("instancias",()=>{
     const local= new Local("A");
     const facturacion= new Facturacion(local,3);
     const calidad= new Calidad(local,4);
-    const distribucion=new Distribucion(local,9);
+    const distribucion=new Distribucion(local,10);
     expect(facturacion.colaEspera).toBe(3);
     expect(calidad.colaEspera).toBe(4);
-    expect(distribucion.colaEspera).toBe(9);
+    expect(distribucion.colaEspera).toBe(10);
     
 
     
